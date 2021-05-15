1d82bc55129842c776c91101db267c8858ff27fa
Renan <renanmendes@pbhub.com.br>
2021-05-15 09:55:37 -0300

    add implementation subscription

--- src/graphql/modules/users/resolvers.js
+++ src/graphql/modules/users/resolvers.js
@@ -10,8 +10,20 @@ export default {
         getUsuarioByAtivado: (_, {ativado}) => User.find(ativado)
     },
     Mutation: {
-        createUser: (_, {data}) => User.create(data),
+        createUser: async (_, {data}, {pubsub}) => {
+            const user = await User.create(data);
+            pubsub.publish('USER_ADDED', {
+                userAdded: user,
+            });
+            return user;
+        },
+        
         updateUser: (_, {id, data}) => User.findByIdAndUpdate(id, data, {new: true}), 
         deleteUser: async (_, {id}) => !!(await User.findByIdAndDelete(id)) // !! força que o retorno seja um boolean
     },
+    Subscription: {
+        userAdded: {
+            subscribe: (obj, args, {pubsub}) => pubsub.asyncIterator('USER_ADDED'),
+        }
+    }
 }
